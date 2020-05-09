481c024f08e36e2db03f4da883bff02c1464fb86
whyn0 <p.gianluca189@gmail.com>
2020-05-09 17:39:01 +0200

    Spostato file

--- src/main/java/proj/map/progettomap1920/adventure/main/AppMain.java
+++ src/main/java/proj/map/progettomap1920/adventure/main/AppMain.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package proj.map.progettomap1920;
+package proj.map.progettomap1920.adventure.main;
 
 /**
  *
