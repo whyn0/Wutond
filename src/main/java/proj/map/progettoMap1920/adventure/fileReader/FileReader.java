8b42211b134a2e37829ce6d0a317fbb66ee56cdc
whyn0 <p.gianluca189@gmail.com>
2020-05-09 17:34:57 +0200

    Creazione scheletro progetto

---
+++ src/main/java/proj/map/progettoMap1920/adventure/fileReader/FileReader.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proj.map.progettoMap1920.adventure.fileReader;
+
+/**
+ *
+ * @author whyno
+ */
+public class FileReader {
+    
+}
---
+++ src/main/java/proj/map/progettomap1920/GameDescription.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proj.map.progettomap1920;
+
+/**
+ *
+ * @author whyno
+ */
+public abstract class GameDescription {
+    
+}
